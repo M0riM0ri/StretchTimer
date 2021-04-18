8d8b052d969c27aba5e7f684501712ff29421e53
gunjo <gunjo2012@gmail.com>
2021-04-18 15:58:20 +0900

    Add home logs help

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,10 @@
+class MainPagesController < ApplicationController
+  def home
+  end
+
+  def logs
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'main_pages/home'
+  get 'main_pages/logs'
+  get 'main_pages/help'
   devise_for :users
   root 'timer#start'
   get 'timer/start'
