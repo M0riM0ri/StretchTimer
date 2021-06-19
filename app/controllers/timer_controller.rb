f6c8e2f82105a6f40e6bcc76877fe7f6425b18d8
gunjo <gunjo2012@gmail.com>
2021-06-19 20:36:27 +0900

    Fix no-db bug

--- app/controllers/timer_controller.rb
+++ app/controllers/timer_controller.rb
@@ -3,8 +3,10 @@ class TimerController < ApplicationController
     if user_signed_in? 
       @new_worktime = current_user.worktimes.build
       @update_worktime = current_user.worktimes.find_by(user_id: current_user.id)
-      if @update_worktime.start_time.present? & @update_worktime.end_time.nil?
-        redirect_to timer_work_path
+      if @update_worktime.present?
+        if @update_worktime.start_time.present? & @update_worktime.end_time.nil?
+          redirect_to timer_work_path
+        end
       end
     end
   end
