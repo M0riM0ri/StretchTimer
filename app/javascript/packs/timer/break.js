e478d12f4b190e706f76f67c1205ab2b03477954
gunjo <gunjo2012@gmail.com>
2021-06-24 23:01:15 +0900

    Add more stretch movie

--- app/javascript/packs/timer/break.js
+++ app/javascript/packs/timer/break.js
@@ -3,12 +3,16 @@ document.title = "休憩中 ~StretchTimer~";
 const movie_id = ["iEda-ZxH_1M", //首の横のストレッチ
                   "cuUU3UOkswg", //首の前のストレッチ
                   "7EYkfn5yLOo", //目の疲れを改善するマッサージ①
+                  "5rczr-DLMPI", //目の疲れを改善するマッサージ②
+                  "t5VUIparXN0", //目の疲れを改善するマッサージ③
                   "hEUxM9BEqGc", //肩甲骨マッサージ
                   "GPbVezfVaiE", //肩甲骨ほぐし①
                   "o9Z3f8V6fn8", //肩甲骨ほぐし②
                   "WRfT5RIMfYU", //肩甲骨ほぐし③
                   "a9TEEliF1w4", //腕回し
                   "h8tu-bWYatw", //肋骨マッサージ
+                  "7gw14FPcNLk", //頚椎モビリゼーション
+                  "GrVxeslWnZ8", //肋骨下マッサージ
                 ];
 
 
