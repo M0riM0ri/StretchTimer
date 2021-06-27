2f0708dd4812b69c1e98a77f9c5841bcb0eb5507
gunjo <gunjo2012@gmail.com>
2021-06-27 16:35:22 +0900

    Add mailer view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,44 @@
 class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  # 確認画面
+  def confirm
+    @contact = Contact.new(contact_params)
+    if @contact.invalid?
+      render :new
+    end
+  end
+　
+  # 入力内容に誤りがあった場合
+  def back
+    @contact = Contact.new(contact_params)
+    render :new
+  end
+
+  # 送信
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      ContactMailer.send_mail(@contact).deliver_now
+      redirect_to done_path
+    else
+      render :new
+    end
+  end
+
+  # 送信完了画面を使用する場合お使いください。
+  def done
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact)
+          .permit(:email,
+                  :name,
+                  :message
+                 )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  root 'timer#index'
 
-  get 'users', to: 'users#sign_up' 
-
+  # Timer
   get 'timer/index'
   get 'timer/work'
   get 'timer/break'
-  
   get 'timer/about'
   get 'timer/timerlog'
-
-  root 'timer#index'
-
   resources :worktimes
+
+  # Login
+  get 'users', to: 'users#sign_up' 
+
+  # Mailer
+  resources :contacts, only: [:new, :create]
+  post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
+  post 'contacts/back', to: 'contacts#back', as: 'back'
+  get 'done', to: 'contacts#done', as: 'done'
+
 end
