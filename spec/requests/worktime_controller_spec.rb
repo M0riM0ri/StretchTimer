22b2a409a48debb49cde2091ec551b20fd891399
gunjo <gunjo2012@gmail.com>
2021-07-12 20:31:26 +0900

    Add worktimes_controller test to update

--- spec/requests/worktime_controller_spec.rb
+++ spec/requests/worktime_controller_spec.rb
@@ -2,27 +2,32 @@ describe WorktimesController, type: :controller do
 
   describe 'POST #worktimes' do
 
-    params = {
-      start_time: 10,
-      pause_time: 0,
-      restart_time: 0,
-      accumulate_time: 0,
-      in_progress: 1,
-      timing: 60,
+    new_params = {
+      start_time: 10, pause_time: 0,
+      restart_time: 0, accumulate_time: 0,
+      in_progress: 1, timing: 60,
     }
 
     it 'creates a new worktime data' do
       @user = create(:user)
       sign_in @user
       expect {
-        post :create, params: { worktime: params }
+        post :create, params: { worktime: new_params }
       }.to change{ Worktime.count }.by 1
+    end
+
+    it 'updates a worktime data' do
+      @user = create(:user)
+      sign_in @user
+      post :create, params: { worktime: new_params }
+      patch :update, params: { id: @user.id, worktime: { end_time: 100 } }
+      expect( @user.worktimes.find_by(user_id: @user.id).end_time ).to eq 100
     end
     
     it 'destroys a worktime data' do
       @user = create(:user)
       sign_in @user
-      post :create, params: { worktime: params }
+      post :create, params: { worktime: new_params }
       expect {
         delete :destroy, params: {id: 1}
       }.to change{ Worktime.count }.by -1
